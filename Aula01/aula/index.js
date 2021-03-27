f296b9994d421e190c57c6a21c4fb22f70595823
Candida Priscilla Nascimento Gomes <candida.gomes@hbsis.com.br>
2021-03-27 19:30:35 -0300

    commit message

--- Aula01/aula/index.js
+++ Aula01/aula/index.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import winston from 'winston';
 import accountsRouter from '../routes/accounts.js';
 import { promises as fs } from 'fs';
 
 const { readFile, writeFile } = fs;
 
-global.fileName = "accounts.json";
+global.fileName = 'accounts.json';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myformat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'mybank.log' }),
+  ],
+
+  format: combine(label({ label: 'mybank' }), timestamp(), myformat),
+});
 
 const app = express();
 app.use(express.json());
@@ -14,7 +29,7 @@ app.use('/account', accountsRouter);
 app.listen(3000, async () => {
   try {
     await readFile(global.fileName);
-    console.log('API SUCESS!');
+    logger.info('API SUCESS!');
   } catch (err) {
     const initialJson = {
       nextId: 1,
@@ -22,10 +37,10 @@ app.listen(3000, async () => {
     };
     writeFile(global.fileName, JSON.stringify(initialJson))
       .then(() => {
-        console.log('API SUCESS and file Created!');
+        logger.info('API SUCESS and file Created!');
       })
       .catch((err) => {
-        console.log(err);
+        logger.error(err);
       });
   }
 });
